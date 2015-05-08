fdc609b0271ef89ef8dc226814285ddcf01e07b5
roman.kagan <roman.kagan@lge.com>
2015-05-07 23:31:42 -0700

    .

--- src/com/romankagan/scratchpad/MaxMin.scala
+++ src/com/romankagan/scratchpad/MaxMin.scala
@@ -7,6 +7,13 @@ object MaxMin {
 
   val grades = List(1,2,3,4,5,3,2,1,5,15,6)
   var max = 0
-  grades.foreach(x)
+  //grades.foreach(x)
+
+
+  val names = List("sid","helder","gina","g","roman","david")
+  val result = names.filter(_.length>2)
+  .map(_.capitalize).
+  reduce(_+","+_)
+  println(result)
 
 }
--- src/com/romankagan/scratchpad/Prototype.scala
+++ src/com/romankagan/scratchpad/Prototype.scala
@@ -13,7 +13,13 @@ object Prototype extends App{
 class Prototype  extends FunSuite {
 
   test("The sum should be 5") {
+    val names = List("sid","helder","gina","g","roman","david")
+    val result = names.filter(_.length>2)
+      .map(_.capitalize).
+      reduce(_+","+_)
+    println(result)
     assert(SimpleSum.get_sum_for_test(2,3) == 5)
+
 
   }
 
